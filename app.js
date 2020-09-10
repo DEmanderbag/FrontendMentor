86894f957dfc31f3f9a4fc6681f93d0489456d69
DEmanderbag <thedemanderbag@outlook.com>
2020-09-10 10:43:27 +0200

    Added JavaScript validation

--- app.js
+++ app.js
@@ -7,44 +7,81 @@ let getPassword = document.getElementById("password");
 let getButton = getForm.querySelector("button");
 
 // Create P element;
-let paragraph = document.createElement("p");
+let paragraph1 = document.createElement("p");
+let paragraph2 = document.createElement("p");
+let paragraph3 = document.createElement("p");
+let paragraph4 = document.createElement("p");
 
 // Get image
-let getImage = document.querySelector(".error-img");
+let getImage = document.querySelector(".img1");
 getImage.style.display = "none";
 
-getTestSection = document.querySelector(".last-name");
-paragraph = document.createElement("p");
-paragraph.textContent = "Last name cannot be empty";
-paragraph.classList.add("errormsg");
-getTestSection.appendChild(paragraph);
+// Get Second image
+let getImage2 = document.querySelector(".img2");
+getImage2.style.display = "none";
 
-getTestSection = document.querySelector(".email");
-paragraph = document.createElement("p");
-paragraph.textContent = "Looks like this is not an email";
-paragraph.classList.add("errormsg");
-getTestSection.appendChild(paragraph);
+// Get Third image
+let getImage3 = document.querySelector(".img3");
+getImage3.style.display = "none";
 
-getTestSection = document.querySelector(".password");
-paragraph = document.createElement("p");
-paragraph.textContent = "Password cannot be empty";
-paragraph.classList.add("errormsg");
-getTestSection.appendChild(paragraph);
+// Get Forth image
+let getImage4 = document.querySelector(".img4");
+getImage4.style.display = "none";
 
-function getValue() {
+function getNameValue() {
   const saveName = getFirstName.value;
-  const saveLast = getLastName.value;
-  const saveEmail = getEmail.value;
-  const savePassword = getPassword.value;
 
-  // console.log(getImage);
   if (saveName == "") {
     getTestSection = document.querySelector(".first-name");
     getFirstName.classList.add("errorborder");
     getImage.style.display = "inline";
-    paragraph.textContent = "First Name cannot be empty";
-    paragraph.classList.add("errormsg");
-    getTestSection.appendChild(paragraph);
-  } else console.log(saveName);
+    paragraph1.textContent = "First Name cannot be empty";
+    paragraph1.classList.add("errormsg");
+    getTestSection.appendChild(paragraph1);
+  }
 }
-getButton.addEventListener("click", getValue);
+
+function getLastNameValue() {
+  const saveLast = getLastName.value;
+
+  if (saveLast == "") {
+    getTestSection = document.querySelector(".last-name");
+    getLastName.classList.add("errorborder");
+    getImage2.style.display = "inline";
+    paragraph2.textContent = "Last name cannot be empty";
+    paragraph2.classList.add("errormsg");
+    getTestSection.appendChild(paragraph2);
+  }
+}
+
+function getEmailValue() {
+  const saveEmail = getEmail.value;
+  let validateEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (saveEmail == "" || !saveEmail.match(validateEmail)) {
+    getTestSection = document.querySelector(".email");
+    getEmail.classList.add("errorborder");
+    getImage3.style.display = "inline";
+    paragraph3.textContent = "Looks like this is not an email";
+    paragraph3.classList.add("errormsg");
+    getTestSection.appendChild(paragraph3);
+  }
+}
+
+function getPasswordValue() {
+  const savePassword = getPassword.value;
+
+  if (savePassword == "") {
+    getTestSection = document.querySelector(".password");
+    getPassword.classList.add("errorborder");
+    getImage4.style.display = "inline";
+    paragraph4.textContent = "Password cannot be empty";
+    paragraph4.classList.add("errormsg");
+    getTestSection.appendChild(paragraph4);
+  }
+}
+
+getButton.addEventListener("click", getNameValue);
+getButton.addEventListener("click", getLastNameValue);
+getButton.addEventListener("click", getEmailValue);
+getButton.addEventListener("click", getPasswordValue);
