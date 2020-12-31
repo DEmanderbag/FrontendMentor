f8a59c9704b6ad79aa8c029da5bdea5f46d09911
DEmanderbag <dusan-milosevic@outlook.com>
2020-12-31 22:56:23 +0100

    Update styles and accordion function

--- faqComponent/app.js
+++ faqComponent/app.js
@@ -2,15 +2,17 @@ const questions = document.querySelector(".questions");
 const question = questions.querySelectorAll(".question__section");
 
 question.forEach(e => {
-  e.addEventListener("click", () => {
+  e.addEventListener("pointerdown", () => {
     let tittle = e.childNodes[1];
     let anwser = e.childNodes[3];
-
+    let image = e.querySelector(".question__section img");
     if(anwser.style.maxHeight){
+      image.style.transform = "rotate(0deg)";
       anwser.style.maxHeight = null;
       tittle.classList.toggle("question__title--open");   
       anwser.classList.toggle("question__anwser--open");
     } else {
+      image.style.transform = "rotate(180deg)";
       anwser.style.maxHeight = anwser.scrollHeight + "px";
       tittle.classList.toggle("question__title--open");  
       anwser.classList.toggle("question__anwser--open");
@@ -18,5 +20,4 @@ question.forEach(e => {
   })
 });
 
-// Change arrow when it's open
-// Close previous accordion if one is already pressed
+// Close previous accordion if one is already pressed 
