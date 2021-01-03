a94a5800fc98e093a88dad73192c25876091f3ed
IlijaRad <ilija.radivojevic237@gmail.com>
2021-01-03 23:59:49 +0100

    Fixed slider bug

--- testimonialsSlider/app.js
+++ testimonialsSlider/app.js
@@ -2,28 +2,27 @@ let currentSlide = 1;
 const slides = document.querySelectorAll(".slider");
 
 function showSlide(slideIndex){
-  console.log(slides);
-
-  if (slideIndex > slides.length) { currentSlide = 1 }
-  if (slideIndex < 1) { currentSlide = slides.length }
-  for (var i = 0; i < slides.length; i++) {
+  if (slideIndex > slides.length) currentSlide = 1;
+  else if (slideIndex < 1) currentSlide = slides.length;
+  else currentSlide = slideIndex;
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
   slides[currentSlide - 1].style.display = 'flex';
 }
 
 function nextSlide() {
-  showSlide(currentSlide += 1);
+  showSlide(currentSlide + 1);
 }
 
 function previousSlide() {
-  showSlide(currentSlide -= 1);
+  showSlide(currentSlide - 1);
 }
 
 showSlide(currentSlide);
 
-const prev = document.querySelector('.btn--prev');
-const next =  document.querySelector('.btn--next');
+const prev = document.querySelectorAll('.btn--prev');
+const next =  document.querySelectorAll('.btn--next');
 
-prev.addEventListener("click", previousSlide);
-next.addEventListener("click", nextSlide);
+prev.forEach(btn => btn.addEventListener("click", previousSlide));
+next.forEach(btn => btn.addEventListener("click", nextSlide));
