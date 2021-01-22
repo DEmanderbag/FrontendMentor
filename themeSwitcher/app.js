a510313b682e571a4ac6e2a6a01e119d5a318757
DEmanderbag <dusan-milosevic@outlook.com>
2021-01-22 17:11:09 +0100

    Fix issue with toggle

--- themeSwitcher/app.js
+++ themeSwitcher/app.js
@@ -1,18 +1,18 @@
+const head = document.querySelector("head");
 const toggle = document.querySelector("input");
 const switchBtn = document.querySelector(".switch");
 
 toggle.addEventListener("click", () => {
   if (toggle.checked === true) {
     switchBtn.style.background = "linear-gradient(to right, var(--toggle001), var(--toggle001))";
-    loadCSSFile("/styles/light.css");
+    loadCSSFile("styles/light.css");
   } else {
     switchBtn.style.background = "linear-gradient(to right, var(--toggle001), var(--toggle002))";
-    loadCSSFile("/styles/dark.css");
+    loadCSSFile("styles/dark.css");
   }
 });
 
 function loadCSSFile(filename) {
-  const head = document.querySelector("head");
   let link = document.createElement("link");
   link.type = "text/css";
   link.rel = "stylesheet";
