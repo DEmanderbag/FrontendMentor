8fce5dd3c03d7f05f41a73b4365618717b64aedf
DEmanderbag <dusan-milosevic@outlook.com>
2021-01-22 17:40:23 +0100

    Fix issue with nav that blocks scroll

--- insureLandingPage/app.js
+++ insureLandingPage/app.js
@@ -4,14 +4,16 @@ const menuLine = document.querySelector(".menu div");
 const menuList = document.querySelector(".list");
 const menuItems = document.querySelectorAll(".list li");
 
-menu.addEventListener("click", () =>{
+menu.addEventListener("click", () => {
   body.classList.toggle("open");
   menuLine.classList.toggle("menu__line--open");
   menuList.classList.toggle("is-open");
-})
+});
 
-menuItems.forEach(e => e.addEventListener("click", () =>{
-  body.classList.toggle("open");
-  menuLine.classList.toggle("menu__line--open");
-  menuList.classList.toggle("is-open");
-}));
+menuItems.forEach((e) =>
+  e.addEventListener("click", () => {
+    body.classList.remove("open");
+    menuLine.classList.toggle("menu__line--open");
+    menuList.classList.toggle("is-open");
+  })
+);
