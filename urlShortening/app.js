9c2e5eb0a5ba60b7072477dd9977bfb15d58f6fe
DEmanderbag <dusan-milosevic@outlook.com>
2021-01-23 22:02:23 +0100

    Request data from API

--- urlShortening/app.js
+++ urlShortening/app.js
@@ -17,3 +17,31 @@ menuItems.forEach((e) =>
     menuList.classList.toggle("is-open");
   })
 );
+
+const generateSection = document.querySelector(".generate");
+const form = document.querySelector("form");
+const formInput = document.querySelector("form input");
+const formBtn = document.querySelector("form button");
+
+formBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let currentValue = formInput.value;
+  getLink(currentValue, currentValue);
+});
+
+function generateLink(currentValue, APIlink) {
+  let shortLink = `<div class="link__section">
+      <p class="link__original"><a href="${currentValue}">${currentValue}</a></p>
+      <p class="link__short"><a href="https://${APIlink}">${APIlink}</a></p>
+      <button class="btn short__btn">Copy</button>
+    </div>`;
+  generateSection.insertAdjacentHTML("beforeend", shortLink);
+}
+
+async function getLink(data, longLink) {
+  const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${data}`);
+  const shortLink = await request.json();
+  console.log(shortLink);
+  const shortLinkData = shortLink.result.short_link2;
+  generateLink(longLink, shortLinkData);
+}
