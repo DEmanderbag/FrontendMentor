8129c4eb17becb4dfd9baf8d500844b3a6cc98d5
DEmanderbag <dusan-milosevic@outlook.com>
2021-01-24 14:01:27 +0100

    Add clipboard support

--- urlShortening/app.js
+++ urlShortening/app.js
@@ -1,3 +1,4 @@
+// Mobile menu
 const body = document.querySelector("body");
 const menu = document.querySelector(".menu");
 const menuLine = document.querySelector(".menu div");
@@ -18,30 +19,60 @@ menuItems.forEach((e) =>
   })
 );
 
+// Form
 const generateSection = document.querySelector(".generate");
 const form = document.querySelector("form");
+const formGroup = document.querySelector(".group");
 const formInput = document.querySelector("form input");
+const formLabel = document.querySelector(".form__label");
 const formBtn = document.querySelector("form button");
 
 formBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let currentValue = formInput.value;
-  getLink(currentValue, currentValue);
+
+  if (currentValue == "") {
+    formGroup.classList.add("group--error");
+    formInput.classList.add("error");
+    formLabel.style.display = "block";
+  } else {
+    formGroup.classList.remove("group--error");
+    formInput.classList.remove("error");
+    formLabel.style.display = "none";
+    formBtn.classList.add("loading");
+    formBtn.textContent = "Generating ...";
+    getLink(currentValue, currentValue);
+  }
 });
 
 function generateLink(currentValue, APIlink) {
   let shortLink = `<div class="link__section">
       <p class="link__original"><a href="${currentValue}">${currentValue}</a></p>
       <p class="link__short"><a href="https://${APIlink}">${APIlink}</a></p>
-      <button class="btn short__btn">Copy</button>
+      <button class="btn short__btn link">Copy</button>
     </div>`;
   generateSection.insertAdjacentHTML("beforeend", shortLink);
+  formInput.value = "";
+  formBtn.classList.remove("loading");
+  formBtn.textContent = "Shorten It!";
 }
 
 async function getLink(data, longLink) {
   const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${data}`);
   const shortLink = await request.json();
-  console.log(shortLink);
   const shortLinkData = shortLink.result.short_link2;
   generateLink(longLink, shortLinkData);
+  getButtons(shortLinkData);
+}
+
+function getButtons(smallLink) {
+  let btnID = document.querySelectorAll(".link");
+  btnID.forEach((e) => {
+    e.addEventListener("click", () => {
+      // Copy Data to the clipboard
+      window.navigator.clipboard.writeText(smallLink);
+      e.classList.add("short__btn--active");
+      e.textContent = "Copied!";
+    });
+  });
 }
