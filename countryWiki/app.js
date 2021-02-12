6aafc5ec0186a1d2972b16d2c838fab46693823a
DEmanderbag <dusan-milosevic@outlook.com>
2021-02-12 16:54:11 +0100

    Add support for dark mode

--- countryWiki/app.js
+++ countryWiki/app.js
@@ -1,16 +1,15 @@
 const countries = document.querySelector(".countries");
+const loading = document.querySelectorAll(".is-loading");
 
 // API to request data;
 const api = `https://restcountries.eu/rest/v2/all?fields=name;capital;region;population;flag`;
 
 async function requestData() {
   try {
-    const request = await fetch(apis);
+    const request = await fetch(api);
     const data = await request.json();
-    console.log(data);
     data.forEach((dataPoint) => {
       const { flag, name, population, region, capital } = dataPoint;
-      // console.log(dataPoint);
       populateData(flag, name, population, region, capital);
     });
   } catch (e) {
@@ -19,16 +18,51 @@ async function requestData() {
 }
 
 function populateData(flag, name, population, region, capital) {
-  countries.innerHTML += `<div class="country">
+  countries.innerHTML += `<article class="country">
         <div class="country__flag">
           <img src="${flag}" alt="Flag of ${name}" loading="lazy">
         </div>
         <div class="country__about">
-          <p class="country__name">${name}</p>
+          <h2 class="country__name">${name}</h2>
           <p class="country__population"><span>Population: </span>${population}</p>
           <p class="country__region"><span>Region: </span>${region}</p>
           <p class="country__capital"><span>Capital: </span>${capital}</p>
         </div>
-      </div>`;
+      </article>`;
 }
+
 requestData();
+
+function removeLoader() {
+  for (const country of loading) {
+    country.classList.add("hidden");
+  }
+}
+
+const head = document.querySelector("head");
+const cta = document.querySelector(".cta");
+
+cta.addEventListener("click", () => {
+  cta.classList.toggle("clicked");
+  if (cta.classList.contains("clicked")) {
+    loadCSSFile("assets/style/light.css");
+  } else {
+    loadCSSFile("assets/style/dark.css");
+  }
+  // e ? loadCSSFile("assets/style/light.css") : ;
+});
+// cta.addEventListener("click", () => {
+//   if (toggle.checked === true) {
+//     loadCSSFile("style/light.css");
+//   } else {
+//     loadCSSFile("style/dark.css");
+//   }
+// });
+
+function loadCSSFile(filename) {
+  let link = document.createElement("link");
+  // link.type = "text/css";
+  link.rel = "stylesheet";
+  link.href = filename;
+  head.appendChild(link);
+}
