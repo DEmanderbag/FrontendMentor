54d1c37fc56ad232e6903d6102f00dfab84ef411
DEmanderbag <dusan-milosevic@outlook.com>
2021-02-12 19:00:15 +0100

    Implement filter by region

--- countryWiki/search.js
+++ countryWiki/search.js
@@ -1,10 +1,24 @@
 const search = document.querySelector("input");
 
 search.addEventListener("keyup", (e) => {
+  if (select.value !== "filter") {
+    select.value = "filter";
+  }
   const searchValue = e.target.value.toLowerCase();
-  console.log(searchValue);
   const filteredCountry = data.filter((country) => {
     return country.name.toLowerCase().includes(searchValue);
+  });
+  displayCountry(filteredCountry);
+});
+
+// Filter by Region
+const select = document.querySelector("select");
+
+select.addEventListener("change", (e) => {
+  const filterData = e.target.value;
+  console.log(filterData);
+  const filteredCountry = data.filter((country) => {
+    return country.region.toLowerCase().includes(filterData);
   });
   displayCountry(filteredCountry);
 });
