2baececf66592cf9ddfdb27138e98134738c0e51
DEmanderbag <dusan-milosevic@outlook.com>
2021-02-23 11:05:25 +0100

    Update job page design

--- githubJobs/job.js
+++ githubJobs/job.js
@@ -2,6 +2,8 @@ const aboutCard = document.querySelector(".about__card");
 const jobAbout = document.querySelector(".job__about");
 const jobGroup = document.querySelector(".job__group");
 const apply = document.querySelector(".apply p");
+const applyNow = document.querySelector(".apply__now");
+console.log(applyNow);
 let jobID = localStorage.getItem("jobID");
 
 const PROXY = "https://morning-refuge-16267.herokuapp.com/";
@@ -14,24 +16,38 @@ async function loadJob(jobData) {
 
 function displayData(data) {
   let companyCard = `<div class="job__logo">
-  <img src="${data.company_logo}" alt="company logo"></div>
+  <img src="${data.company_logo}" alt="company logo">
+  </div>
+    <div class="company__info">
       <h1 class="about__company">${data.company}</h1>
       <p class="about__site">${data.company_url.substr(12)}</p>
+    </div>
       <a href="${data.company_url}" class="link link--cta">Company Site</a>`;
   console.log(data.description);
   console.log(data);
 
   aboutCard.innerHTML = companyCard;
 
-  let companyData = `<h1 class="job__title">${data.title}</h1>
-        <p class="job__date">${calculateTime(data.created_at)} . <span
-            class="job__position">${data.type}</span></p>
-        <p class="job__location">${data.location}</p>
-        <button class="cta load-more full--width">Apply Now</button>`;
+  let companyData = `
+  <div class="job__more">
+    <h1 class="job__title">${data.title}</h1>
+      <p class="job__date">${calculateTime(data.created_at)} . <span class="job__position">${
+    data.type
+  }</span></p>
+      <p class="job__location">${data.location}</p>
+  </div>
+        <a href="#apply"><button class="cta load-more full--width">Apply Now</button></a>`;
 
   jobGroup.innerHTML = companyData;
   jobAbout.innerHTML = data.description;
   apply.innerHTML = data.how_to_apply;
+
+  const jobTitle = `
+  <div class="company__apply">
+    <p class="job__title">${data.title}</p>
+    <p class="about__company">${data.company}</p>
+  </div>`;
+  applyNow.insertAdjacentHTML("afterbegin", jobTitle);
 }
 
 loadJob(jobID);
