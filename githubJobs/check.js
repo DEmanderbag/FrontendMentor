187c4e7fadeccd678e45a02e609a271b1ee22d56
DEmanderbag <dusan-milosevic@outlook.com>
2021-02-27 23:57:21 +0100

    Better support for dark mode

--- githubJobs/check.js
+++ githubJobs/check.js
@@ -17,3 +17,40 @@ function checkImage(logo) {
     return logo;
   }
 }
+
+// Dark mode toggle
+let themeMode = localStorage.getItem("themeMode");
+const swtich = document.querySelector("input[name=theme]");
+
+const darkMode = () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+  swtich.checked = true;
+  localStorage.setItem("themeMode", "dark");
+};
+
+const lighMode = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+  swtich.checked = false;
+  localStorage.setItem("themeMode", "light");
+};
+
+if (themeMode === "dark") {
+  darkMode();
+}
+
+swtich.addEventListener("change", () => {
+  if (swtich.checked) {
+    transition();
+    darkMode();
+  } else {
+    transition();
+    lighMode();
+  }
+});
+
+let transition = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
