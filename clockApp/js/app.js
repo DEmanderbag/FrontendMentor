1e2980a45b6f4002e2a7e134b5dee5f2e167e2e5
DEmanderbag <dusan-milosevic@outlook.com>
2021-03-24 23:36:38 +0100

    Update service worker path

--- clockApp/js/app.js
+++ clockApp/js/app.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/sw.js")
+    .register("sw.js")
     .then((registration) => console.log("Service worker registered"))
     .catch((error) => console.log("Service worker not registered", error));
 }
