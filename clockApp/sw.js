8d101b06a0ebb40b7b9b3598a2e65e74ae056e0d
DEmanderbag <dusan-milosevic@outlook.com>
2021-03-25 18:13:31 +0100

    Fix path issue

--- clockApp/sw.js
+++ clockApp/sw.js
@@ -1,24 +1,23 @@
-const staticCacheName = "site-static-v5";
+const staticCacheName = "site-static-v2";
 const assets = [
-  "/",
   "index.html",
-  "/css/background.css",
-  "/assets/mobile/bg-image-daytime.jpg",
-  "/assets/mobile/bg-image-nighttime.jpg",
-  "/assets/tablet/bg-image-daytime.jpg",
-  "/assets/tablet/bg-image-nighttime.jpg",
-  "/assets/desktop/bg-image-daytime.jpg",
-  "/assets/desktop/bg-image-nighttime.jpg",
-  "/assets/desktop/icon-arrow-up.svg",
-  "/assets/desktop/icon-moon.svg",
-  "/assets/desktop/icon-refresh.svg",
-  "/assets/desktop/icon-sun.svg",
-  "/js/app.js",
-  "/js/mode.js",
-  "/js/moreData.js",
-  "/js/quote.js",
-  "/js/time.js",
-  "/css/main.css",
+  "css/background.css",
+  "assets/mobile/bg-image-daytime.jpg",
+  "assets/mobile/bg-image-nighttime.jpg",
+  "assets/tablet/bg-image-daytime.jpg",
+  "assets/tablet/bg-image-nighttime.jpg",
+  "assets/desktop/bg-image-daytime.jpg",
+  "assets/desktop/bg-image-nighttime.jpg",
+  "assets/desktop/icon-arrow-up.svg",
+  "assets/desktop/icon-moon.svg",
+  "assets/desktop/icon-refresh.svg",
+  "assets/desktop/icon-sun.svg",
+  "js/app.js",
+  "js/mode.js",
+  "js/moreData.js",
+  "js/quote.js",
+  "js/time.js",
+  "css/main.css",
   "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap",
   "https://fonts.gstatic.com/s/inter/v3/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7W0Q5n-wU.woff2",
 ];
@@ -26,14 +25,10 @@ const assets = [
 // install event
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(staticCacheName)
-      .then((cache) => {
-        cache.addAll(assets);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
   );
 });
 
@@ -47,10 +42,10 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cacheResponse) => {
-      return cacheResponse || fetch(event.request);
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
     })
   );
 });
