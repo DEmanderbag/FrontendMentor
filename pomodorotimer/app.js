7f47453ed60655f27e4e463d75274ce664bbabc4
DEmanderbag <dusan-milosevic@outlook.com>
2021-04-05 19:29:59 +0200

    Add ability to change between timers

--- pomodorotimer/app.js
+++ pomodorotimer/app.js
@@ -10,3 +10,61 @@ appSettings.addEventListener("click", () => {
 closeSettings.addEventListener("click", () => {
   settings.classList.remove("is-open");
 });
+
+// Timer options
+const timerOptions = document.querySelectorAll(".app__options input");
+let timerName;
+
+// Timer names and time in minutes
+let pomodoro = 25;
+let shortBreak = 5;
+let longBreak = 15;
+let inverval;
+let time;
+let isRunning = false;
+
+// Timer time
+let timer = document.querySelector(".app__timer h1");
+let timerState = document.querySelector(".app__timer p");
+const timerScreen = document.querySelector(".app__container");
+
+timerScreen.addEventListener("click", () => {
+  if (isRunning) return;
+
+  isRunning = true;
+  timerState.innerHTML = "pause";
+  inverval = setInterval(updateTimer, 1000);
+});
+
+timerOptions.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    timerName = e.path[1].innerText;
+    if (timerName == "pomodoro") {
+      time = pomodoro * 60;
+      updateTimer();
+    } else if (timerName == "short break") {
+      time = shortBreak * 60;
+      updateTimer();
+    } else {
+      time = longBreak * 60;
+      updateTimer();
+    }
+  });
+});
+
+function updateTimer() {
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  minutes = pad(minutes);
+  seconds = pad(seconds);
+
+  timer.innerHTML = `${minutes}:${seconds}`;
+  time--;
+  if (time < 0) {
+    clearInterval(inverval);
+  }
+}
+
+function pad(number) {
+  return number < 10 ? "0" + number : number;
+}
